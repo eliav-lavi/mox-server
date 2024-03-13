b58453847ff21c59571433d315f8e483f4fccc7e
shai.ungar <shai.ungar@riskified.com>
2024-03-13 22:52:33 +0200

    Add an option to store binary data in an endpoint encoded in base64
    1. add attribute return_value_binary to indicate if the return value is binary
    2. add a method that decodes the data from base64 to binary

--- models/endpoint.rb
+++ models/endpoint.rb
@@ -1,6 +1,7 @@
 require 'dry-struct'
 require 'json'
 require 'erb'
+require 'base64'
 
 require_relative '../types'
 require_relative 'endpoint_request'
@@ -14,6 +15,7 @@ module Models
     attribute :verb, Types::String
     attribute :path, Types::String
     attribute :return_value, Types::Any
+    attribute :return_value_binary, Types::Bool.default(false)
     attribute :min_response_millis?, Types::Integer
     attribute :max_response_millis?, Types::Integer
     attribute :status_code, Types::Integer.default(200)
@@ -29,8 +31,18 @@ module Models
       raise TemplateEvaluationError.new(e.message)
     end
 
+    def return_value_binary
+      Base64.decode64(return_value)
+    rescue StandardError => e
+      raise InvalidBinaryError.new(e.message)
+    end
+
     def name
       "#{verb} #{path}"
+    end
+
+    def return_value_binary?
+      self[:return_value_binary]
     end
 
     def self.build(obj, id: nil)
